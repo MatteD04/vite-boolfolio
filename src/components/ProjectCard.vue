a4ada42803ba1c861f55f4c6851dd618763946b1
MatteD04 <matteo.dantonio04@gmail.com>
2024-06-11 17:56:17 +0200

    feat: add project's card

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        infoProject: Object
+    }
+}
+</script>
+
+<template>
+    <div class="card my-3">
+        <div class="card-body">
+            <h5 class="card-title">{{ infoProject.name }}</h5>
+            <div v-if="infoProject.type">
+                <strong>Type</strong>:
+                {{ infoProject.type.name }}
+            </div>
+            <p v-if="infoProject.summary" class="card-text">{{ infoProject.summary }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+</style>
--- src/components/project.vue
+++ src/components/project.vue
@@ -1,11 +1,42 @@
 <script>
+import axios from 'axios';
+import ProjectCard from './ProjectCard.vue';
+
 export default {
-    name: 'project'
+    name: 'project',
+    data() {
+        return {
+            projects: []
+        };
+    },
+    components: {
+        ProjectCard
+    },
+    methods: {
+        getProjectsApi() {
+            axios.get('http://127.0.0.1:8000/api/projects')
+            .then((response) => {
+                this.projects = response.data.results;
+            });
+        }
+    },
+    mounted() {
+        this.getProjectsApi();
+    }
 }
 </script>
 
 <template>
-    ciao
+    <div class="container">
+        <div>
+            <h1>I MIEI PROGETTI</h1>
+        </div>
+        <div class="row row-cols-3">
+            <div class="col" v-for="project in projects" :key="project.id">
+                <ProjectCard :infoProject="project"></ProjectCard>
+            </div>
+        </div>
+    </div>
 </template>
 
-<style></style>
+<style scoped lang="scss"></style>
