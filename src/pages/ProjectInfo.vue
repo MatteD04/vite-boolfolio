313d5cfde08d030202a8eac9d48b4cf4a1fa1531
MatteD04 <matteo.dantonio04@gmail.com>
2024-06-12 18:03:51 +0200

    feat: add projectinfo's page

---
+++ src/pages/ProjectInfo.vue
@@ -0,0 +1,13 @@
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'ProjectInfo',
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1>nome progetto</h1>
+    </div>
+</template>
--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import AppHome from './pages/AppHome.vue';
 import AppAbout from './pages/AppAbout.vue';
 import project from './pages/project.vue';
+import ProjectInfo from './pages/ProjectInfo.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -21,6 +22,11 @@ const router = createRouter({
             path: '/project',
             name: 'project',
             component: project
+        },
+        {
+            path: '/project/:slug',
+            name: 'project-info',
+            component: ProjectInfo
         }
     ]
 });
